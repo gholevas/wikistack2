861e8faba0c4d78ed99affaa803e5d82018c88cc
George Holevas <gholevas1@gmail.com>
2016-01-27 10:04:16 -0500

    added model tests

--- test/page.spec.js
+++ test/page.spec.js
@@ -1,14 +1,33 @@
-describe('Route Tests', function() {
-    describe('A subcategory', function() {
-        xit('tests something', function() {});
-        xit('tests another aspect of the same thing', function() {});
+describe('Page model', function() {
+
+    describe('Validations', function() {
+        xit('errors without title', function() {});
+        xit('errors without content', function() {});
     });
-    describe('A different subcategory', function() {});
-});
-describe('Model Tests', function() {
-    describe('A subcategory', function() {
-        xit('tests something', function() {});
-        xit('tests another aspect of the same thing', function() {});
+
+    describe('Statics', function() {
+        describe('findByTag', function() {
+            xit('gets pages with the search tag', function() {});
+            xit('does not get pages without the search tag', function() {});
+        });
     });
-    describe('A different subcategory', function() {});
+
+    describe('Methods', function() {
+        describe('findSimilar', function() {
+            xit('never gets itself', function() {});
+            xit('gets other pages with any common tags', function() {});
+            xit('does not get other pages without any common tags', function() {});
+        });
+    });
+
+    describe('Virtuals', function() {
+        describe('route', function() {
+            xit('returns the url_name prepended by "/wiki/"', function() {});
+        });
+    });
+
+    describe('Hooks', function() {
+        xit('it sets urlTitle based on title before validating', function() {});
+    });
+
 });
