d3a262f68f5337452c41e77f14c2d9a04c955095
George Holevas <gholevas1@gmail.com>
2016-01-27 10:00:52 -0500

    edited package

---
+++ test/page.spec.js
@@ -0,0 +1,14 @@
+describe('Route Tests', function() {
+    describe('A subcategory', function() {
+        xit('tests something', function() {});
+        xit('tests another aspect of the same thing', function() {});
+    });
+    describe('A different subcategory', function() {});
+});
+describe('Model Tests', function() {
+    describe('A subcategory', function() {
+        xit('tests something', function() {});
+        xit('tests another aspect of the same thing', function() {});
+    });
+    describe('A different subcategory', function() {});
+});
